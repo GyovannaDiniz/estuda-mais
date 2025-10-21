4d1840a357cc059c1b2b3aea6a6ec87329a2cea8
Gyovanna <gyovanna.diniz@academico.ifpb.edu.br>
2025-10-21 02:02:37 +0000

    alterações em simulados

--- app/(tabs)/simulados.tsx
+++ app/(tabs)/simulados.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@react-navigation/elements';
 import React from 'react';
-import {Dimensions, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Dimensions, Image, StyleSheet, ScrollView, Text, TouchableOpacity, View} from 'react-native';
 
 
 
@@ -13,7 +13,7 @@ export default function Login() {
                 source={require('@/assets/images/logoEstuda.png')}
                 style={styles.imagem}
             />
-
+        <ScrollView>
             <View style={styles.inputContainer}>
                 <Text style={styles.texto}>
                     45 questões de matemática e suas tecnologias
@@ -24,12 +24,22 @@ export default function Login() {
             </View>
 
             <View style={styles.inputContainer}>
-
+                <Text style={styles.texto}>
+                    45 questões de matemática e suas tecnologias
+                </Text>
+                    <TouchableOpacity style={styles.botao} onPress={() => console.log('clicado')}>
+                        <Text style={styles.textoBotao}>Entrar</Text>
+                    </TouchableOpacity>
             </View>
             <View style={styles.inputContainer}>
-
+                <Text style={styles.texto}>
+                    45 questões de matemática e suas tecnologias
+                </Text>
+                    <TouchableOpacity style={styles.botao} onPress={() => console.log('clicado')}>
+                        <Text style={styles.textoBotao}>Entrar</Text>
+                    </TouchableOpacity>
             </View>
-
+        </ScrollView>
         </View>
     );
 }
@@ -52,24 +62,35 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: width * 0.8,
         height: height * 0.2,
-        backgroundColor: '#ccc',
+        backgroundColor: '#FEADA6',
         borderRadius: width * 0.03,
         marginTop: height * 0.02,
+        justifyContent: 'flex-end',
+        padding: height * 0.02,
     },
 
     texto: {
+        marginBottom: height * 0.01, 
+        color: '#000',
+        fontSize: 16,
        
     },
 
     botao: {
-        backgroundColor: '#e2afafff',
+        backgroundColor: '#FEDBD8',
         width: width * 0.2,
         height: height * 0.05,
-        borderRadius: width * 0.03,
-    },
+        borderRadius: width * 0.02,
+        marginRight: width * 0.03,   
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center',
+},
+
 
     textoBotao: {
-        color: '#fff',
+        color: '#000',
+        
         
     }
 
