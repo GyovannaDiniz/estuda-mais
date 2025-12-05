37e311bd8aa148fcb266b793af38ed9d76c237e7
Gyovanna <gyovanna.diniz@academico.ifpb.edu.br>
2025-12-05 10:58:28 +0000

    adicionei a função de cadastro com validação e integração com API

--- app/(tabs)/cadastro.tsx
+++ app/(tabs)/cadastro.tsx
@@ -1,88 +1,133 @@
 import React, { useState } from 'react';
-import { Dimensions, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Dimensions, Image, StyleSheet, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
+import { router } from "expo-router";
 
 const { width, height } = Dimensions.get('window');
 
-
-export default function cadastro(){
+export default function cadastro() {
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+
+    async function handleRegister() {
+        if (!nome || !email || !senha) {
+            Alert.alert("Erro", "Preencha todos os campos!");
+            return;
+        }
+
+        try {
+            const resp = await fetch("https://silver-barnacle-x5p6qv5rvx9gh6449-3000.app.github.dev/api/autenticacao/signup",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        nome,
+                        email,
+                        senha,
+                        role: "aluno"
+                       
+                    })
+                }
+            );
+
+            const respostaBruta = await resp.text();
+            console.log("Status da resposta:", resp.status);
+            console.log("Resposta do servidor (texto bruto):", respostaBruta);
+
+            let respostaJSON = {};
+            try {
+                respostaJSON = JSON.parse(respostaBruta);
+            } catch (e) {
+                console.log("Erro ao converter pra JSON:", e);
+            }
+
+            if (!resp.ok) {
+                if (resp.status === 409) {
+                    Alert.alert("Atenção", "E-mail já cadastrado!");
+                    return;
+                }
+                Alert.alert("Erro", respostaJSON.error|| respostaJSON.message || "Erro ao cadastrar.");
+                return;
+            }
+
+            Alert.alert("Sucesso", "Cadastro realizado!");
+            router.replace("/inicio");
+
+        } catch (error) {
+            console.log("Erro no fetch:", error);
+            Alert.alert("Erro", "Não foi possível conectar ao servidor.");
+        }
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.circulo}>
-                 <Text style={styles.titulo}>Cadastre-se</Text>
-            <View style={styles.campoComIcone}>
-                <Image 
-                    source={require('@/assets/images/userNome.png')}
-                    style={styles.iconeDentro}
-                />
-                <TextInput
-                    style={styles.inputComIcone}
-                    placeholderTextColor={'#ccc'}
-                    placeholder='Nome'//nomezinho escrito dentro do campo
-                    value={nome}
-                    onChangeText={setNome} //vai servir para atualizar o valor
-                 
-                />
-            </View>
+                <Text style={styles.titulo}>Cadastre-se</Text>
 
-            <View style={styles.campoComIcone}>
-                <Image 
-                    source={require('@/assets/images/e-mail.png')}
-                    style={styles.iconeDentro}
-                />
-                <TextInput
-                    style={styles.inputComIcone}
-                    placeholderTextColor={'#ccc'}
-                    placeholder='Email'
-                    value={email}
-                    onChangeText={(setEmail)}
-                />
-            </View>
+                <View style={styles.campoComIcone}>
+                    <Image
+                        source={require('@/assets/images/userNome.png')}
+                        style={styles.iconeDentro}
+                    />
+                    <TextInput
+                        style={styles.inputComIcone}
+                        placeholderTextColor={'#ccc'}
+                        placeholder='Nome'
+                        value={nome}
+                        onChangeText={setNome}
+                    />
+                </View>
 
-            <View style={styles.campoComIcone}>
-                <Image
-                    source={require('@/assets/images/cadeado.png')}
-                    style={styles.iconeDentro}
-                />
-                <TextInput
-                    style={styles.inputComIcone}
-                    placeholderTextColor={'#ccc'}
-                    placeholder='Senha'
-                    value={senha}
-                    secureTextEntry={true}
-                    onChangeText={(setSenha)}
-                />              
-            </View>
-               
-                <Link href="/inicio" asChild>
-                    <TouchableOpacity style={styles.botao} onPress={() => console.log('clicado')}>
-                        <Text style={styles.texto}>Cadastre-se</Text>
-                    </TouchableOpacity>
-                </Link>
+                <View style={styles.campoComIcone}>
+                    <Image
+                        source={require('@/assets/images/e-mail.png')}
+                        style={styles.iconeDentro}
+                    />
+                    <TextInput
+                        style={styles.inputComIcone}
+                        placeholderTextColor={'#ccc'}
+                        placeholder='Email'
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+                </View>
 
-            <View style={styles.linhaComTexto}>
-                <View style={styles.linha}/>
-                <Text style={styles.ou}>OU</Text>
-                <View style={styles.linha}/>
+                <View style={styles.campoComIcone}>
+                    <Image
+                        source={require('@/assets/images/cadeado.png')}
+                        style={styles.iconeDentro}
+                    />
+                    <TextInput
+                        style={styles.inputComIcone}
+                        placeholderTextColor={'#ccc'}
+                        placeholder='Senha'
+                        secureTextEntry={true}
+                        value={senha}
+                        onChangeText={setSenha}
+                    />
+                </View>
+
+                <TouchableOpacity style={styles.botao} onPress={handleRegister}>
+                    <Text style={styles.texto}>Cadastre-se</Text>
+                </TouchableOpacity>
+
+                <View style={styles.linhaComTexto}>
+                    <View style={styles.linha}/>
+                    <Text style={styles.ou}>OU</Text>
+                    <View style={styles.linha}/>
+                </View>
             </View>
-            <View>
-            {/* <TouchableOpacity style={styles.botao} onPress={() => console.log('clicado')}>
-                <Text style={styles.texto}>Google</Text>
-            </TouchableOpacity> */}
         </View>
-        </View>
-        </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,                   // usa toda altura da tela
-        justifyContent: 'center',  // centraliza verticalmente
-        alignItems: 'center',      // centraliza horizontalmente
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: '#fff',
         padding: width * 0.20,
     },
@@ -97,22 +142,12 @@ const styles = StyleSheet.create({
     },
 
     titulo: {
-        textAlign:'center', 
+        textAlign:'center',
         fontSize: width * 0.05,
-        marginBottom: height * 0.02, //espaço abaixo de cada campo
+        marginBottom: height * 0.02,
         fontWeight:'bold',
     },
 
-    input: {
-        height: height * 0.06,
-        borderColor: '#C1C1C1',
-        backgroundColor: '#F7FAFB',
-        borderWidth: width * 0.001, // espessura da borda
-        marginBottom:height * 0.03,
-        paddingHorizontal: width * 0.1, //espaço interno dos lados(na horizontal)
-        borderRadius: width * 0.10,
-    },
-    
     botao: {
         backgroundColor:'#FAD0C4',
         padding: 10,
@@ -131,18 +166,17 @@ const styles = StyleSheet.create({
     },
 
     linhaComTexto: {
-        alignItems: 'center', 
-        flexDirection: 'row',  // coloca os itens em linha (horizontal)
-        marginVertical: height * 0.02, //// espacinho acima e abaixo da linha com "OU"
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginVertical: height * 0.02,
         marginHorizontal: width * 0.06,
     },
 
-      linha: {
+    linha: {
       height: height * 0.002,
       width: width * 0.2,
       backgroundColor: '#000',
       marginVertical: height * 0.02,
-      
     },
 
     ou: {
@@ -159,9 +193,7 @@ const styles = StyleSheet.create({
         height: height * 0.06,
         paddingHorizontal: width * 0.04,
         marginBottom: height * 0.02,
-        
     },
-    
 
     iconeDentro: {
         width: width * 0.05,
@@ -169,9 +201,8 @@ const styles = StyleSheet.create({
         marginRight: 10,
     },
 
-     inputComIcone: {
+    inputComIcone: {
         flex: 1,
         fontSize: 16,
     },
-})
-
+});
